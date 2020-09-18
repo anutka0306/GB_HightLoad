148685590d80e55c2c7c6bbfcea8905084fbc9f1
anutka0306 <anutka0306@mail.ru>
2020-09-18 11:30:03 +0300

    index.php and image

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+echo "Hello World <br>";
+
+function isEven($number){
+        if($number < 0){
+                $number = abs($number);
+        }
+        if($number%2==0){
+                return $number . " is even";
+        }
+        return $number . " isn't enven";
+}
+
+echo isEven(5). "<br>";
+echo isEven(6). "<br>";
+    
