d68d63900eecbba8b975512a313ff6ac8bc4f5e1
anutka0306 <anutka0306@mail.ru>
2020-10-15 21:51:28 +0300

    lesson7

---
+++ lesson7/limonade.php
@@ -0,0 +1,24 @@
+<?php
+require_once('vendor/autoload.php');
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Exception\AMQPProtocolChannelException;
+use PhpAmqpLib\Message\AMQPMessage;
+
+try {
+
+    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+    $channel->queue_declare('Limonade', false, true, false, false);
+
+    $msg = new AMQPMessage($_POST['type']);
+    $channel->basic_publish($msg, '', 'Limonade');
+    $channel->close();
+    $connection->close();
+}
+catch (AMQPProtocolChannelException $e){
+    echo $e->getMessage();
+}
+catch (AMQPException $e){
+    echo $e->getMessage();
+}
